8c154461c13b1f5136af033286bfc353e0b164cd
Berkay Eşer <berkay9822@gmail.com>
2024-04-18 18:27:54 +0300

    Repository created
    Database created in pgAdmin 4. Database linked to VS Code.

---
+++ sql_queries/date_practice_1.sql
@@ -0,0 +1,8 @@
+/*
+
+    Write a query to find the average salary both yearly (salary_year_avg)and hourly
+    (salary_hour_avg) for jobs that were posted after June 1, 2023. Group the results by
+    job schedule type.
+
+*/
+
---
+++ sql_queries/date_practice_2.sql
@@ -0,0 +1,7 @@
+/*
+
+    Write a query to count  the number  of job postings for each month in 2023, adjusting the
+    job_posted_date to be 'America/New York' time zone before extracting (hint) the mount.
+    Assume the job_posted_date is stored in UTC. Group by and order by the month.
+
+*/
